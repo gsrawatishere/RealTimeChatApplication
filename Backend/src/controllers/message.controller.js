0f79da31df52e62179ed8b6fe936095fb4237620
gsrawatishere <gaurishankarrawat78@gmail.com>
2025-06-25 00:41:53 +0530

    Implemented message service

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import cloudinary from "../lib/cloudinary";
 const prisma = new PrismaClient();
 
 export const getAllUsers = async (req,res)=>{
@@ -22,5 +23,69 @@ export const getAllUsers = async (req,res)=>{
     } catch (error) {
         console.log("Error in finding all users!", error);
         res.status(500).json({msg : "Unable to find all users!",error});
+    }
+}
+
+export const getMessages = async (req,res) => {
+    try {
+            const {id:userToChatId}  = req.params;
+            const myId = req.user.id;
+
+            const messages = await prisma.message.findMany({
+               where : {
+                OR: [
+                    {senderId : myId,receiverId :userToChatId },
+                    {senderId : userToChatId, receiverId : myId}
+                ]
+               },
+               orderBy : {
+                timestamp : 'asc'  // oldest first
+               }
+               
+            })
+        res.status(200).json({messages})
+
+    } catch (error) {
+         console.log("Error in getMessages route",error);
+         res.status(500).json({msg : "Unable to Load messages!",error});
+    }
+}
+
+export const sendMessage = async (req,res) => {
+    try {
+         const {text,image} = req.body;
+         const {id: receiverId} = req.params;
+         const senderId = req.user.id;
+
+         if (!text && !image) {
+            return res.status(400).json({ msg: "Empty Message!" });
+         }
+
+         let imageUrl = "";
+         if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image,{
+                folder : "chat_images"
+            });
+            imageUrl = uploadResponse.secure_url;
+         }
+         
+         const newMessage = await prisma.message.create({
+            data : {
+                senderId,
+                receiverId,
+                text : text || "", 
+                image : imageUrl || "",
+            },
+         })
+ // logic here -------- socket.io
+
+         res.status(201).json({
+            message: newMessage
+          });
+      
+    }
+     catch (error) {
+         console.log("Error in sendmessage route",error);
+         res.status(500).json({msg : "Unable to send message!", error})
     }
 }
--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { verifyUser } from "../middleware/verify.js";
-import { getAllUsers } from "../controllers/message.controller.js";
+import { getAllUsers, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users",verifyUser,getAllUsers);
+router.get("/:id",verifyUser,getMessages);
+router.post("/send/:id",verifyUser,sendMessage)
 
 
 export default router;
